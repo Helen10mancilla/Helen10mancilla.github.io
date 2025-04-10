9b7d8a2afc57ffd82713d57c43f56a98f46a5e46
Helen orejuela mancilla <135240673+Helen10mancilla@users.noreply.github.com>
2025-04-10 16:22:38 -0500

    Actualizacion pagina web
    Realice una actualización en mi pagina principal con una mejor interfaz visual

--- logica.js
+++ logica.js
@@ -1,3 +1,16 @@
-function mostrarMensaje() {
-    alert("Hola Mundo");
-}
+const sections = document.querySelectorAll('.fade-in');
+const options = {
+  threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+    }
+  });
+}, options);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
